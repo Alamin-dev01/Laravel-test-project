8864c248c54a2cf3f6380383ed0a828b3cfbc540
Md. Al-Amin <alamin2001islam@gmail.com>
2024-06-05 13:28:52 +0600

    2nd push

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,4 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/index', [App\Http\Controllers\DashboardController::class, 'index'])->name('admin.dashboard');
 Route::get('/payment', [App\Http\Controllers\paymentController::class, 'payment'])->name('admin.payment');
+Route::get('/items', [App\Http\Controllers\paymentController::class, 'items'])->name('admin.items');
